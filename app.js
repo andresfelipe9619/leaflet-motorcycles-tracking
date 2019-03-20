528469096892e718a0036b0175d89e19737b61ff
andresfelipe9619 <andresfelipe9619@gmail.com>
2019-03-20 15:17:40 -0500

    Return firts working query

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var express = require("express"); // require Express
-var router = express.Router(); // setup usage of the Express router engine
-
+const express = require("express"); // require Express
+const router = express.Router(); // setup usage of the Express router engine
+const app = express();
 /* PostgreSQL and PostGIS module and connection setup */
 const { Client, Query } = require("pg");
 
 // Setup connection
-var username = "docker"; // sandbox username
-var password = "docker"; // read only privileges on our table
-var host = "localhost:25432";
-var database = "mototrip"; // database name
-var conString =
+const username = "docker"; // sandbox username
+const password = "docker"; // read only privileges on our table
+const host = "localhost:25432";
+const database = "mototrip"; // database name
+const conString =
   "postgres://" + username + ":" + password + "@" + host + "/" + database; // Your Database Connection
 
 // Set up your database query to display GeoJSON
-var coffee_query =
-  "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM clientes As lg) As f) As fc";
+const coffee_query =
+  "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, nombre)) As properties FROM clientes As lg) As f) As fc";
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -33,6 +33,11 @@ router.get("/data", function(req, res) {
     res.send(result.rows[0].row_to_json);
     res.end();
   });
+});
+
+app.use(router);
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
 
 module.exports = router;
